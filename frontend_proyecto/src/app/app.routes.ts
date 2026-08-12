d92c3634d57322aad9106cc3ca0b3ba2f46e799c
F4Harold <haroldarciniegas08@gmail.com>
2026-08-12 17:03:53 -0500

    conponente de centro de ayuda

--- frontend_proyecto/src/app/app.routes.ts
+++ frontend_proyecto/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CentroAyudaConponent } from './pages/centro-ayuda/centro-ayuda';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'ayuda',
+        component:CentroAyudaConponent
+    },
+];
