c97b20e4c3a8aa1422fcbddb188d08b6fe332bc5
F4Harold <haroldarciniegas08@gmail.com>
2026-08-13 14:45:54 -0500

    creacions del conponente de linea de Ayuda

--- frontend_proyecto/src/app/app.routes.ts
+++ frontend_proyecto/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
-import { CentroAyudaConponent } from './pages/centro-ayuda/centro-ayuda';
+import { CentroAyudaConponent } from './pages/soporte/centro-ayuda/centro-ayuda';
+import { LineaAyudaComponent } from './pages/soporte/linea-ayuda/linea-ayuda';
 
 export const routes: Routes = [
     {
         path:'ayuda',
         component:CentroAyudaConponent
     },
+    {
+        path:'linea-ayuda',
+        component:LineaAyudaComponent
+    }
 ];
